7b95380ee660f5d31d50261860d373c8cde6babf
EndZz-2 <endzz2209@gmail.com>
2020-05-15 22:01:45 +0200

    start index + grid system

--- index.php
+++ index.php
@@ -5,16 +5,40 @@
 
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil</title>
     <link rel="icon" href="img/logo.png" type="image/x-icon">
     <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/grid.css">
 </head>
+
 <body>
     <?php
         require 'header.php';
     ?>
+    <img class="scrolldown" src="img/scroll.svg" alt="scroll">
+    <div class="banner">
+        <img class="AYI" src="img/AreYouIn.svg" alt="AreYouIn">
+        <img class="AYI-back" src="img/AreYouIn-.svg" alt="?">
+    </div>
+    <div class="content-index">
+        <div class="first-container row">
+            <div class="col-1">
+                <div class="video"></div>
+            </div>
+            <div class="col-2">
+                <div class="text">
+                    <h1><span class="titre-dot">.</span>Description entreprise</h1>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing
+                        elitr, sed diam nonumy eirmod tempor invidunt ut
+                        labore et dolore magna aliquyam.</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
