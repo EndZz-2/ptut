9f0c9ab2c39dfeae74bd61c6b0076dc4b8ba4d4a
hugo-lg-twi <legrand.hugo83@hotmail.fr>
2020-05-21 11:50:24 +0200

    ajout partenaires et conception sur index 21/05 ( css à completer)

--- index.php
+++ index.php
@@ -6,39 +6,88 @@
 <!DOCTYPE html>
 <html lang="fr">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Accueil</title>
-    <link rel="icon" href="img/logo.png" type="image/x-icon">
-    <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="css/grid.css">
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Accueil</title>
+        <link rel="icon" href="img/logo.png" type="image/x-icon">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/grid.css">
+        <link rel="stylesheet" href="css/table.css">
+    </head>
 
-<body>
-    <?php
+    <body>
+        <?php
         require 'header.php';
     ?>
-    <img class="scrolldown" src="img/scroll.svg" alt="scroll">
-    <div class="banner">
-        <img class="AYI" src="img/AreYouIn.svg" alt="AreYouIn">
-        <img class="AYI-back" src="img/AreYouIn-.svg" alt="?">
-    </div>
-    <div class="content-index">
-        <div class="first-container row">
-            <div class="col-1">
-                <div class="video"></div>
-            </div>
-            <div class="col-2">
-                <div class="text">
-                    <h1><span class="titre-dot">.</span>Description entreprise</h1>
-                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing
-                        elitr, sed diam nonumy eirmod tempor invidunt ut
-                        labore et dolore magna aliquyam.</p>
+        <img class="scrolldown" src="img/scroll.svg" alt="scroll">
+        <div class="banner">
+            <img class="AYI" src="img/AreYouIn.svg" alt="AreYouIn">
+            <img class="AYI-back" src="img/AreYouIn-.svg" alt="?">
+        </div>
+        <div class="content-index">
+            <div class="first-container row">
+                <div class="col-1">
+                    <div class="video"></div>
+                </div>
+                <div class="col-2">
+                    <div class="text">
+                        <h1>
+                            <span class="titre-dot">.</span>Description entreprise</h1>
+                        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+                            eirmod tempor invidunt ut labore et dolore magna aliquyam.</p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</body>
+        <div class="#">
+            <div class="#">
+                <div class="#">
+                    <div >
+                        <h1>
+                            .nospartenaires</h1>
+                        <table border="1" class="tabcenter" cellspacing="1" style="width:200px ">
+                            <tbody>
+                                <tr>
+                                    <td><img src="img/test.png"/>
+                                    </td>
+                                    <td><img src="img/test.png"/>
+                                    </td>
+                                    <td><img src="img/test.png"/>
+                                    </td>
+                                    <td><img src="img/test.png"/>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
 
-</html>
+                </div>
+            </div>
+        </div>
+
+        <div class="content-index">
+            <div class="first-container row">
+                <div class="col-1">
+                    <div class="conception">
+                        <h1 >
+                            .conception
+                        </h1>
+                        <p>> Création d'une identité visuelle
+                        </p>
+                        <p>
+                            > Création d'un site web vitrine</p>
+                        <p>> Rédaction d'article
+                        </p>
+                    </div>
+                    <div class="col-1">
+
+                        <a class="btn" href="contact.php">Services</a>
+
+                    </div>
+                </div>
+            </div>
+
+        </body>
+
+    </html>
